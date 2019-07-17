da2ce8795ec27fd7f5a13fd66a53d0b80219f3d9
Gianluca Coidessa <gianluca.coidessa@yahoo.it>
2019-07-17 23:45:39 +0200

    hanged in version 2 addes $1 input and exit statuhanged in version 2 addes $1 input and exit status

--- random.sh
+++ random.sh
@@ -1,6 +1,25 @@
-printf '%d:\n' 10
-for i in $(seq 0 9)
-do
-    echo $RANDOM
-    printf $i
-done
+#!/bin/bash
+
+#wrong_opt = 5
+
+if [ $1 -gt 0 ]
+
+#if (($1 > 0))
+   
+then
+    
+    printf '%d:\n' $1
+
+    for i in $(seq 1 $1)
+    do
+        echo $RANDOM
+
+    done
+    
+else
+    printf '%s\n' $(basename $0)
+    echo "Unknown $1 variable input"
+    echo "To perform the script you need to pass an integer input"
+    #printf '%s\n' "Exit status $wrong_opt"
+    printf '%s\n' "Exit status 5"
+fi
